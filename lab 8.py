60385f93a7705397ee4ec367025f7b09016a9641
Marco Gallo <mgallo811@gmail.com>
2024-11-22 16:41:25 +0100

    description

---
+++ lab 8.py
@@ -0,0 +1,9 @@
+#In this part I'll describe the Simpson paradox using  pyAgrum
+#le probabilità a noi fornite sono le seguenti:
+# #gender: [0.5, 0.5]
+# drug|gender = male; (with, without): [0.25, 0.75] 
+# drug|gender = female; (with, without): [0.75, 0.25] 
+# result|gender  = male, drug = without; (sick, healed): [0.60, 0.40]
+# result|gender  = male, drug = with; (sick, healed): [0.80, 0.20]
+# result|gender  = female, drug = without; (sick, healed): [0.20, 0.80]
+# result|gender  = female, drug = with; (sick, healed): [0.30, 0.70]
